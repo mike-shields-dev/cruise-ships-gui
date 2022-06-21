a87f55d0e5d943289a7d42dee2650e02e5be1c95
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-21 10:03:10 +0100

    Refactor Port and Ship test suites
    - Adds beforeEach blocks to Port and Ship test suites
    - Reformats Ship and Port test files

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,61 +1,59 @@
 const { Port } = require("../src/Port")
 
 describe("Port", () => {
+  let port, ship1, ship2
+
+  beforeEach(() => {
+    port = new Port("Liverpool")
+    ship1 = {}
+    ship2 = {}
+  })
+
   it("can be instantiated", () => {
-    const port = new Port()
     expect(port).toBeInstanceOf(Port)
     expect(port).toBeInstanceOf(Object)
   })
 
   it("has a name property that can be set at instantiation", () => {
-    const port = new Port("Liverpool")
     expect(port.name).toBe("Liverpool")
   })
 
   it("has a ships property", () => {
-    const port = new Port()
     expect(port).toHaveProperty("ships")
   })
 
   it("ships property is initialised as an empty array", () => {
-    const port = new Port()
     expect(port.ships).toEqual([])
   })
 
   it("has an addShip method", () => {
-    const port = new Port()
     expect(port.addShip).toBeInstanceOf(Function)
   })
 
   it("addShip adds the given ship to the ships array", () => {
-    const port = new Port()
-    const ship = { name: "Boaty McBoatface" }
+    const ship = {}
     port.addShip(ship)
+
     expect(port.ships).toContain(ship)
   })
 
   it("has removeShip method", () => {
-    const port = new Port()
-
     expect(port.removeShip).toBeInstanceOf(Function)
   })
 
   it("removeShip throws an error if the ships array is empty", () => {
-    const port = new Port()
-
     expect(() => port.removeShip()).toThrowError("No ships to remove")
   })
 
   it("removeShip removes the given ship from the ships array", () => {
-    const port = new Port()
-    const ship1 = { name: "Boaty McBoatface" }
-    const ship2 = { name: "The Black Pearl" }
-    
+    const ship1 = {}
+    const ship2 = {}
+
     port.addShip(ship1)
     port.addShip(ship2)
-
     port.removeShip(ship1)
 
     expect(port.ships).not.toContain(ship1)
+    expect(port.ships).toContain(ship2)
   })
 })
--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -3,124 +3,86 @@ const { Port } = require("../src/Port")
 const { Itinerary } = require("../src/Itinerary")
 
 describe("Ship", () => {
-  const firstPort = new Port("Liverpool")
-  const secondPort = new Port("Dublin")
-  const lastPort = new Port("New York")
-  const itinerary = new Itinerary([firstPort, secondPort, lastPort])
+  describe("with ports and an itinerary", () => {
+    let firstPort, secondPort, lastPort, itinerary, ship
+    
+    beforeEach(() => {
+      firstPort = new Port("Liverpool")
+      secondPort = new Port("Dublin")
+      lastPort = new Port("New York")
+      itinerary = new Itinerary([firstPort, secondPort, lastPort])
+      ship = new Ship(itinerary)
+    })
 
-  it("can be instantiated", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship).toBeInstanceOf(Ship)
-    expect(ship).toBeInstanceOf(Object)
-  })
-
-  it("has a previousPort", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship).toHaveProperty("previousPort")
-  })
-
-  it("previousPort is initialised to null", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship.previousPort).toBeNull()
-  })
-
-  it("has a currentPort property", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship).toHaveProperty("currentPort")
-  })
-
-  it("currentPort is initialised to the first port in the itinerary", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship.currentPort).toBe(firstPort)
-  })
-
-  it("ship gets added to ships array of currentPort at instantiation", () => {
-    const ship = new Ship(itinerary)
-
-    expect(firstPort.ships).toContain(ship)
-  })
-
-  it("has a setSail method", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship.setSail).toBeInstanceOf(Function)
-  })
-
-  it("setSail throws an error if the ship is not docked", () => {
-    const ship = new Ship(itinerary)
-    ship.currentPort = null
-
-    expect(() => ship.setSail()).toThrowError("The ship is not docked")
-  })
-
-  it("setSail sets currentPort to null", () => {
-    const ship = new Ship(itinerary)
-
-    ship.setSail()
-
-    expect(ship.currentPort).toBeNull()
-  })
-
-  it("setSail removes ship from ports array of previousPort", () => {
-    const ship = new Ship(itinerary)
-
-    ship.setSail()
-
-    expect(firstPort.ships).not.toContain(ship)
-  })
-
-  it("ship can't setSail when at last port in itinerary", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship.currentPort).toBe(firstPort)
-
-    ship.setSail()
-    ship.dock()
-
-    expect(ship.currentPort).toBe(secondPort)
-
-    ship.setSail()
-    ship.dock()
-
-    expect(ship.currentPort).toBe(lastPort)
-
-    expect(() => ship.setSail()).toThrowError(
-      "The ship has arrived at its final destination"
-    )
-  })
-
-  it("has a dock method", () => {
-    const ship = new Ship(itinerary)
-
-    expect(ship.dock).toBeInstanceOf(Function)
-  })
-
-  it("dock throws an error if the ship is already docked", () => {
-    const ship = new Ship(itinerary)
-
-    expect(() => ship.dock()).toThrowError("Ship is already docked")
-  })
-
-  it("dock sets currentPort to the next port in the itinerary", () => {
-    const ship = new Ship(itinerary)
-
-    ship.setSail()
-    ship.dock()
-
-    expect(ship.currentPort).toBe(secondPort)
-  })
-
-  it("dock adds ship to ships array of currentPort", () => {
-    const ship = new Ship(itinerary)
-
-    ship.setSail()
-    ship.dock()
-
-    expect(secondPort.ships).toContain(ship)
+    it("can be instantiated", () => {
+      expect(ship).toBeInstanceOf(Ship)
+      expect(ship).toBeInstanceOf(Object)
+    })
+  
+    it("has a previousPort", () => {
+      expect(ship).toHaveProperty("previousPort")
+    })
+  
+    it("previousPort is initialised to null", () => {  
+      expect(ship.previousPort).toBeNull()
+    })
+  
+    it("has a currentPort property", () => {  
+      expect(ship).toHaveProperty("currentPort")
+    })
+  
+    it("currentPort is initialised to the first port in the itinerary", () => {  
+      expect(ship.currentPort).toBe(firstPort)
+    })
+  
+    it("ship gets added to ships array of currentPort at instantiation", () => {  
+      expect(firstPort.ships).toContain(ship)
+    })
+  
+    it("has a setSail method", () => {  
+      expect(ship.setSail).toBeInstanceOf(Function)
+    })
+  
+    it("setSail throws an error if the ship is not docked", () => {
+      ship.currentPort = null  
+      expect(() => ship.setSail()).toThrowError("The ship is not docked")
+    })
+  
+    it("setSail sets currentPort to null", () => {
+      ship.setSail()
+      expect(ship.currentPort).toBeNull()
+    })
+  
+    it("setSail removes ship from ports array of previousPort", () => {
+      ship.setSail()
+      expect(firstPort.ships).not.toContain(ship)
+    })
+  
+    it("ship can't setSail when at last port in itinerary", () => {
+      ship.currentPort = lastPort
+      expect(() => ship.setSail()).toThrowError(
+        "The ship has arrived at its final destination"
+      )
+    })
+  
+    it("has a dock method", () => {
+      expect(ship.dock).toBeInstanceOf(Function)
+    })
+  
+    it("dock throws an error if the ship is already docked", () => {
+      expect(() => ship.dock()).toThrowError("Ship is already docked")
+    })
+  
+    it("dock sets currentPort to the next port in the itinerary", () => {
+      ship.setSail()
+      ship.dock()
+      expect(ship.currentPort).toBe(secondPort)
+    })
+  
+    it("dock adds ship to ships array of currentPort", () => {
+      ship.setSail()
+      ship.dock()
+      expect(secondPort.ships).toContain(ship)
+    })
   })
 })
